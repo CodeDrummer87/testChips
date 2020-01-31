b7d655d90b9d1310a011d9a058b0a9cfd9293e53
CodeDrummer <code.drummer87@gmail.com>
2020-02-01 05:41:35 +0600

    GetValue() method created + ShowAvailableChips & CheckChipValue()

--- chips.cpp
+++ chips.cpp
@@ -16,7 +16,7 @@ const int MAX = 6;
 struct Chip
 {
 	int value_;
-	bool isUsed;
+	bool isUsed_;
 };
 
 class Player
@@ -39,11 +39,73 @@ public:
 		for (int i = 0; i < MAX; i++)
 		{
 			stack[i].value_ = i + 1;
-			stack[i].isUsed = false;
+			stack[i].isUsed_ = false;
 		}
 	}
+	//.:: Methods_of_class
+	int GetValue(bool);
+	void ShowAvailableChips();
+	bool CheckChipValue(int, bool);
 };
-
+//////////////////////////////////////////////////////
+bool Player:: CheckChipValue(int choice, bool player)
+{
+	if(choice > 0 && choice < 7)
+	{
+		if(!stack[choice - 1].isUsed_)
+			return true;
+		else
+		{
+			if(player)
+				cout << ".:: Фишка была использована ранее!" << endl;
+			return false;
+		}
+	}
+	else
+	{
+		cout << ".:: Такой фишки не существует!" << endl;
+		return false;
+	}
+}
+//----------------------------------------------------
+void Player:: ShowAvailableChips()
+{
+	cout << "[";
+	for (int i = 0; i < MAX; i++)
+	{
+		if(!stack[i].isUsed_)
+			cout << stack[i].value_ << ' ';
+	}
+	cout << "]: ";
+}
+//----------------------------------------------------
+int Player::GetValue(bool player)
+{
+	int choice = 0;
+	if(player)
+	{
+		while(true)
+		{
+			cout << ".:: Выберите фишку из доступных ";
+			ShowAvailableChips();
+			cin >> choice;
+			if(CheckChipValue(choice, true))
+				break;
+		}
+	}
+	else
+	{
+		while(true)
+		{
+			choice = rand() % 6 + 1;
+			if(CheckChipValue(choice, false))
+				break;
+		}
+	}
+	stack[choice - 1].isUsed_ = true;
+	return choice;
+}
+//----------------------------------------------------
 void charline(char = '-', int = 80);
 //////////////////////////////////////////////////////
 int main()
@@ -63,9 +125,11 @@ int main()
 
 	srand(time(NULL));
 
-	while(round > 6)
+	while(round < 6)
 	{
-
+		cout << setw(75) << ".:: раунд " << ++round << endl;
+		playerChoice = player -> GetValue(true);
+		enemyChoice = enemy -> GetValue(false);
 	}
 
 	_getch();
